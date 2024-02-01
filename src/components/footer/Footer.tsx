1dc7268ba0648eaa9293da4779d610b73fae70aa
Santosh Yadav <ymanoutube@gmail.com>
2024-02-02 00:47:25 +0545

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ import "./footer.scss"
 const Footer = () => {
   return (
     <div className="footer">
-      <span>lamadmin</span>
-      <span>© Lama Dev Admin Dashboard</span>
+      <span>Dr.Fish</span>
+      <span>© Dr.Fish Admin Dashboard</span>
     </div>
   )
 }
