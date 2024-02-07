9b2dc3962e174651f3514090914e0f0b0a590d15
Santosh Yadav <ymanoutube@gmail.com>
2024-02-07 13:10:45 +0545

    area chart

--- src/components/bigChartBox/BigChartBox.tsx
+++ src/components/bigChartBox/BigChartBox.tsx
@@ -55,14 +55,24 @@ const data = [
   },
 ];
 
+const updatedData = data.map(item => ({
+  name: item.name,
+  nitrogen: item.books,
+  oxygen: item.clothes,
+  potassium: item.electronic,
+}));
+
+
+
+
 const BigChartBox = () => {
   return (
     <div className="bigChartBox">
-      <h1>Revenue Analytics</h1>
+      <h1>Chemical Compositions</h1>
       <div className="chart">
         <ResponsiveContainer width="99%" height="100%">
           <AreaChart
-            data={data}
+            data={updatedData}
             margin={{
               top: 10,
               right: 30,
@@ -75,21 +85,21 @@ const BigChartBox = () => {
             <Tooltip />
             <Area
               type="monotone"
-              dataKey="electronic"
+              dataKey="nitrogen"
               stackId="1"
               stroke="#8884d8"
               fill="#8884d8"
             />
             <Area
               type="monotone"
-              dataKey="clothes"
+              dataKey="potassium"
               stackId="1"
               stroke="#82ca9d"
               fill="#82ca9d"
             />
             <Area
               type="monotone"
-              dataKey="books"
+              dataKey="oxygen"
               stackId="1"
               stroke="#ffc658"
               fill="#ffc658"
