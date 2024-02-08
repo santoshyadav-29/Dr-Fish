38dd88543ab905ea9fbfe3e2d268d3a0cafa1716
Santosh Yadav <ymanoutube@gmail.com>
2024-02-08 20:11:07 +0545

    scss

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,15 +1,15 @@
-import Single from "../../components/single/Single"
-import { singleUser } from "../../data"
+import Single from "../../components/single/Single";
+import { singleUser } from "../../data";
+import "./user.scss";
 
 const User = () => {
-
   //Fetch data and send to Single Component
-  
+
   return (
     <div className="user">
-      <Single {...singleUser}/>
+      <Single {...singleUser} />
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
